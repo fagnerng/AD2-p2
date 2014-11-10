6d638b1cf4fcb736bf0b514560eac1eb5c321c89
Fagner Gomes <fagnerngomes@hotmail.com>
2014-11-10 20:18:03 -0300

    question 1 no relatorio html

--- loadTables.R
+++ loadTables.R
@@ -5,6 +5,19 @@ subset_tracks_per_year <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_trac
 subset_artist_location <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_artist_location.csv")
 subset_unique_artists <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_unique_artists.csv")
 artist_term <- read.csv("~/Documents/AD2/Problema 2/Dados/artist_term.csv")
+year_term <- read.csv("~/AD2-p2/Dados/year-term.csv")
+
+#windows
+year_term <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/year-term.csv")
+artist_term <- read.csv("C:/Users/fagnerng/git/AD2/Problema 2/Dados/artist_term.csv")
+artists <- read.table("C:/Users/fagnerng/git/AD2-p2/Dados/artists.csv", header=TRUE, quote="\"")
+similarity <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/similarity.csv")
+songs <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/songs.csv")
+subset_artist_location <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_artist_location.csv")
+subset_tracks_per_year <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_tracks_per_year.csv")
+subset_unique_artists <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_unique_artists.csv")
+
+
 songs <- merge(songs, artist_term, "artist_id")
 songs <- subset(songs, songs$term == "rock" | songs$term == "indie" | songs$term == "pop" | songs$term == "jazz" | songs$term == "hip hop")
 summary(songs)
@@ -18,10 +31,19 @@ songs$track_id <- NULL
 songs$title <- NULL
 songs$song_id <- NULL
 songs$release <- NULL
+#///////////////////////////////Questao 1//////////////////////////////////////////////
+# 1 - é possivel acompanhar o crescimento do generos ou da musica em si!?
+# R- em busca de tentar acompanha a evoluçao dos generos musicais( mais listados), 
+# é possivel notar que entre 1920 e 1950 musica nao era algo ta produtivo, talvez
+# pelo fato de radio nao ser tao popular, ou ate mesmo perido de guerra / crise.
+# porem, apos esse  periodo a musica comessa a se popularizar, entrando na era Disco
+# Epoca em que era comum sair para se divertir a noite e escultar alguma musica de seu 
+# interesse.
+# Com a revolucao digital, surgimento do CD, o ramo musical começa a decolar,
+# onde apresenta uma crescimento jamais visto.
 
 install.packages("ggplot2")
 library("ggplot2")
-year_term <- read.csv("~/AD2-p2/Dados/year-term.csv")
 p <- ggplot() 
 p <- p +  geom_line(data = year_term, aes(x = year.term, y = rock, color = "Rock")) 
 p <- p + geom_line(data = year_term, aes(x = year.term, y = pop, color = "Pop")) 
@@ -30,7 +52,21 @@ p <- p + geom_line(data = year_term, aes(x = year.term, y = jazz, color = "Jazz"
 p <- p + geom_line(data = year_term, aes(x = year.term, y = hiphop, color = "Hip Hop")) 
 p <- p + xlab('Years') 
 p <- p + ylab('Count')
-p <- p + labs(color="TAGS", size = 20)
+p <- p + labs(color="TAGS")
 p <- p + scale_y_log10()
-p <- p +geom_bar() 
+p <- p + geom_bar() 
+p <- p + geom_text()
+p <- p + scale_x_continuous(breaks=c(1920,1930,1940,1950,1960,1970,1980,1990,2000,2010))
+p <- p + scale_y_continuous(breaks=c(0,20,40,60,80,100,120,140,160,180,200,220,240))
 p
+
+
+
+#/////////////////////////////////////////////////////////////////////////////////////////#
+artists <- read.table("C:/Users/fagnerng/git/AD2-p2/Dados/artists.csv", header=TRUE, quote="\"")
+similarity <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/similarity.csv")
+songs <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/songs.csv")
+subset_artist_location <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_artist_location.csv")
+subset_tracks_per_year <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_tracks_per_year.csv")
+subset_unique_artists <- read.csv("C:/Users/fagnerng/git/AD2-p2/Dados/subset_unique_artists.csv")
+
