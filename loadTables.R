c495ea665c09cf9321067d3ba8e267ded7df061e
Fagner Gomes <fagner.gomes@embedded.ufcg.edu.br>
2014-11-10 14:17:35 -0300

    update de tabelas

--- loadTables.R
+++ loadTables.R
@@ -1,8 +1,25 @@
 artists <- read.table("~/Documents/AD2/Problema 2/Dados/artists.csv", header=TRUE, quote="\"")
 songs <- read.csv("~/Documents/AD2/Problema 2/Dados/songs.csv")
 similarity <- read.csv("~/Documents/AD2/Problema 2/Dados/similarity.csv")
-subset_tracks_per_year <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_tracks_per_year.csv", header=FALSE)
-subset_artist_location <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_artist_location.csv", header=FALSE)
-subset_unique_artists <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_unique_artists.csv", header=FALSE)
-song_with_years <- subset(songs, songs$year > 0)
-song_with_years < song_with_years[order(song_with_years$year) , ]
+subset_tracks_per_year <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_tracks_per_year.csv")
+subset_artist_location <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_artist_location.csv")
+subset_unique_artists <- read.csv("~/Documents/AD2/Problema 2/Dados/subset_unique_artists.csv")
+artist_term <- read.csv("~/Documents/AD2/Problema 2/Dados/artist_term.csv")
+songs <- subset(songs, songs$term == "rock" | songs$term == "indie" | songs$term == "pop" | songs$term == "jazz" | songs$term == "hip hop")
+summary(songs)
+songs$artist_mbid <- NULL
+songs$artist_name <- NULL
+songs$duration <- NULL
+songs$artist_familiarity <- NULL
+songs$artist_hotttnesss <- NULL
+songs$artist_id <- NULL
+songs$track_id <- NULL
+songs$title <- NULL
+songs$song_id <- NULL
+songs$release <- NULL
+
+library("ggplot2")
+
+a <- ggplot(data = songs, aes(x = year, y = count(term)))
+a <- a + geom_line()
+a
